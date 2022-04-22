bd2bec97de7b824fc2aec9d894bd44afeb01cfe9
Juan Carlos Trimino <juancarlos@trimino.com>
2022-04-22 15:44:37 -0500

    Add to metadata crypto.

--- metadata/src/index.js
+++ metadata/src/index.js
@@ -11,6 +11,7 @@ const mongodbClient = require('mongodb').MongoClient;
 const amqp = require('amqplib');
 const bodyParser = require('body-parser');
 const winston = require('winston');
+const { randomUUID } = require('crypto');
 
 /******
 Globals
